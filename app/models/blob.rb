1e585ba0441295a28a1be4400250f9da61030c3d
sultan-algarbi <sultanye8@gmail.com>
2024-10-12 09:28:31 +0300

    model

--- app/models/blob.rb
+++ app/models/blob.rb
@@ -1,2 +1,8 @@
 class Blob < ApplicationRecord
+  # Set the primary key for the model
+  self.primary_key = "id"
+
+  # Validations
+  validates :id, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9_-]+\z/, message: "only allows alphanumeric characters, hyphens, and underscores" }
+  validates :size, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
