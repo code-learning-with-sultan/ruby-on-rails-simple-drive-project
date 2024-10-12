523988c26c08e4afa24d55091a0556dbac3e04a2
sultan-algarbi <sultanye8@gmail.com>
2024-10-12 09:28:52 +0300

    database

--- db/migrate/20241010211502_create_blobs.rb
+++ db/migrate/20241010211502_create_blobs.rb
@@ -1,11 +1,8 @@
 class CreateBlobs < ActiveRecord::Migration[7.2]
   def change
-    create_table :blobs do |t|
-      t.string :id
-      t.text :data
+    create_table :blobs, id: false do |t|
+      t.string :id, primary_key: true
       t.integer :size
-      t.string :storage_backend
-      t.datetime :created_at
 
       t.timestamps
     end
